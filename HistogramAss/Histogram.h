3bd6d610edf2338bf153b469b80fd19800740a33
ofer <ofer.sheffer@gmail.com>
2017-01-25 15:53:10 +0200

    rm addWithCuda example leftovers

--- HistogramAss/Histogram.h
+++ HistogramAss/Histogram.h
@@ -3,6 +3,4 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
 cudaError_t histogramWithCuda(long* hist, const int* largeArr, const int histSize, const int arrSize);
