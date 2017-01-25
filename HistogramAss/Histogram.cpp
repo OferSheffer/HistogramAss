b794a59029e47f040df8332346fbdfcfaf4c8992
ofer <ofer.sheffer@gmail.com>
2017-01-25 09:03:34 +0200

    TODO list

--- HistogramAss/Histogram.cpp
+++ HistogramAss/Histogram.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	if (myid == MASTER)
 	{
-
+		//init variables
 		const int ARR_SIZE = 300000;
 		const int VALUES_RANGE = 256;
 		int* largeArr = (int*)calloc(ARR_SIZE, sizeof(int));
@@ -33,7 +33,24 @@ int main(int argc, char *argv[])
 		//largeArr[i] = 1;
 
 
+		int*  valCountArr = (int*)calloc(VALUES_RANGE, sizeof(int));
+		int*  dataArr = (int*)malloc(ARR_SIZE * sizeof(int));
+		int** counterMat = (int**)malloc(4 * sizeof(int*)); //init to my 4 core laptop
+		for (int i = 0; i < 4; i++)
+		{
+			counterMat[i] = (int*)calloc(VALUES_RANGE, sizeof(int));
+		}
 
+		// TODO send half the array to slave for calculations
+
+
+
+		// TODO use openMP for first half
+
+
+
+
+		// TODO use CUDA for second half
 		cudaError_t cudaStatus = histogramWithCuda(hist, largeArr, ARR_SIZE, VALUES_RANGE);
 		if (cudaStatus != cudaSuccess) {
 			fprintf(stderr, "addWithCuda failed!");
@@ -42,6 +59,18 @@ int main(int argc, char *argv[])
 
 		printf("Histogram sample = {%d,%d,%d,%d,%d}\n",
 			hist[0], hist[1], hist[2], hist[3], hist[4]);
+
+
+
+
+		// TODO receive results from slave
+
+
+		// TODO combine results with openMP
+
+
+		// TODO display final histogram
+
 
 		free(largeArr);
 
