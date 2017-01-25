3a9a2bf66085b17a3c41ded5751a4d368f20a58c
ofer <ofer.sheffer@gmail.com>
2017-01-25 10:41:50 +0200

    omp first quarter

--- HistogramAss/Histogram.cpp
+++ HistogramAss/Histogram.cpp
@@ -2,6 +2,7 @@
 #include "device_launch_parameters.h"
 
 #include <mpi.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,10 +10,14 @@
 #include "Histogram.h"
 
 #define MASTER 0
+#define NO_OMP_THREADS 4		// OMP: 4 core laptop
 
 int main(int argc, char *argv[])
 {
+	const int VALUES_RANGE = 256;
 	int numprocs, myid;
+	long hist[VALUES_RANGE] = { 0 };
+	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -21,33 +26,39 @@ int main(int argc, char *argv[])
 
 	if (myid == MASTER)
 	{
-		//init variables
+		//init MASTER variables
 		const int ARR_SIZE = 300000;
-		const int VALUES_RANGE = 256;
 		int* largeArr = (int*)calloc(ARR_SIZE, sizeof(int));
-		long hist[VALUES_RANGE] = { 0 };
 
+		//init largeArr values:
 		srand(time(NULL));
 		for (long i = 0; i < ARR_SIZE; i++)
 			largeArr[i] = rand() % VALUES_RANGE;
-		//largeArr[i] = 1;
+			//largeArr[i] = 1;
 
 
-		int*  valCountArr = (int*)calloc(VALUES_RANGE, sizeof(int));
-		int*  dataArr = (int*)malloc(ARR_SIZE * sizeof(int));
-		int** counterMat = (int**)malloc(4 * sizeof(int*)); //init to my 4 core laptop
-		for (int i = 0; i < 4; i++)
-		{
-			counterMat[i] = (int*)calloc(VALUES_RANGE, sizeof(int));
-		}
+		int* ompHistogram = (int*)calloc(VALUES_RANGE, sizeof(int));
+		int* ompCounterArr = (int*)calloc(NO_OMP_THREADS * VALUES_RANGE , sizeof(int));   // each thread given pseudo-private VALUES_RANGE
 
 		// TODO send half the array to slave for calculations
 
 
 
-		// TODO use openMP for first half
+		// use openMP for first half
+		// TODO apply same code in slave
+		#pragma omp parallel for 
+		for (int i = 0; i < ARR_SIZE/4; ++i)
+			ompCounterArr[omp_get_thread_num()*VALUES_RANGE + largeArr[i]]++;			// each thread collects histogram data
 
+		#pragma omp parallel for num_threads(NO_OMP_THREADS)
+		for (int i = 0; i < VALUES_RANGE; ++i)
+			for (int ix = 0; ix < NO_OMP_THREADS; ix++)
+			{
+				ompHistogram[i] += ompCounterArr[ix*VALUES_RANGE + i];					// threads aggregate histogram data for specific histogram values
+			}
 
+		printf("OMP Histogram sample = {%d,%d,%d,%d,%d}\n",
+			ompHistogram[0], ompHistogram[1], ompHistogram[2], ompHistogram[3], ompHistogram[4]);
 
 
 		// TODO use CUDA for second half
